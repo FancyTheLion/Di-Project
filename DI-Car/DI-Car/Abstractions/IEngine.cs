1ca57947253a0ca7a5100fed6b4eb2afae9aa196
Fancy <lev2001504@gmail.com>
2023-09-19 15:32:25 +0200

    Подкручен какой то интерфейс

---
+++ DI-Car/DI-Car/Abstractions/IEngine.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DI_Car.Abstractions
+{
+    internal interface IEngine
+    {
+        /// <summary>
+        /// Проверка мотора на работоспособность
+        /// </summary>
+        void CheckEnginePerfomance();
+
+        /// <summary>
+        /// Завести мотор
+        /// </summary>
+        void StartEngine();
+
+        /// <summary>
+        /// Заглушить мотор
+        /// </summary>
+        void StopEngine();
+    }
+}
